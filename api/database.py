b777066c0f2b95c533fed5ddf3bd052fc9430047
Vaishnavi Metkar <vaishanavi.metkar2802@gmail.com>
2026-07-25 10:34:22 +0530

    Use environment variable for database connection

--- api/database.py
+++ api/database.py
@@ -1,10 +1,16 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root123@localhost:5432/loan_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
